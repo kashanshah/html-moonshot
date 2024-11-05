45313703ee5a137cff7cf241562bd0552c439df1
Kashan Shah <kashanshah@hotmail.com>
2024-11-05 16:44:34 -0500

    init

---
+++ index.php
@@ -0,0 +1,265 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./css/main.css?v=6" />
+    <script src="js/main.js"></script>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="./images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="./images/favicon/site.webmanifest">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Luckiest+Guy&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+    <title>moonshot</title>
+</head>
+<body>
+<!-- Hero section -->
+
+<section class="cs-hero">
+    <div class="stars"></div>
+    <div class="twinkling"></div>
+    <div class="cs-inner-container">
+        <div class="cs-logo-container" data-aos="zoom-in">
+<!--            <img src="./images/logo.png" alt="" />-->
+        </div>
+        <img src="./images/cartoon-xolo.gif" alt="$MOONSHOOT" class="logo-anim" />
+        <div class="floating-logo-title">
+            <h1>MOONSHOT</h1>
+        </div>
+        <div class="cs-hero--container">
+            <div class="cs-hero--content">
+                <h1 class="cs-text-white" data-aos="zoom-in" data-aos-delay="100">
+                    <span class="cs-text-red">$MOONSHOT</span> is your ticket to lunar riches—no PVP, just the thrill of liftoff!
+                </h1>
+                <div class="cs-hero--action">
+                    <a href="https://dexscreener.com/tron/TLEoEmSpdHX2XQfJKqg1szY3YM7Adjhe19"
+                       target="_blank"
+                       class="cs-main-button"
+                       data-aos="zoom-in"
+                       data-aos-delay="200"
+                    >
+                        BUY $MOONSHOT
+                    </a>
+                    <div
+                        class="cs-hero-socials"
+                        data-aos="zoom-in"
+                        data-aos-delay="300"
+                    >
+                        <a class="cs-social-icon" href="https://x.com/moonshot" target="_blank"
+                        ><img src="./images/x.png" alt=""
+                            /></a>
+                        <a href="https://t.me/moonshotportal" target="_blank" class="cs-social-icon"
+                        ><img src="./images/telegram.png" alt=""
+                            /></a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="cs-img-wrapper" data-aos="slide-up">
+            <img src="./images/bois.png" alt="" />
+        </div>
+    </div>
+</section>
+<section class="cs-carousel">
+    <div class="cs-carousel--content">
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+    </div>
+    <div class="cs-carousel--content">
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+    </div>
+    <div class="cs-carousel--content">
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+    </div>
+    <div class="cs-carousel--content">
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+        <span>$MOONSHOT</span><span>$MOONSHOT</span><span>$MOONSHOT</span>
+    </div>
+</section>
+<section class="cs-helmet">
+    <div class="cs-inner-container">
+<!--        <h2 class="cs-text-white-washed" data-aos="zoom-in" data-aos-delay="100">Woof Woof</h2>-->
+        <div class="cs-helmet--container">
+            <div class="cs-helmet--gif" data-aos="zoom-in" data-aos-delay="100">
+                <img src="./images/news.jpg" alt="" />
+            </div>
+            <div
+                class="cs-helmet--content"
+                data-aos="zoom-in"
+                data-aos-delay="100"
+            >
+                <p>BUCKLE UP! <span class="cs-text-red">$MOONSHOOT</span> is ready to defy gravity and saar past competition. $Moonshoot is primed for a meteoric rise.</p>
+                <a href="https://t.me/moonshotportal" target="_blank" class="cs-main-button">Click me, quick!</a>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="wenmoon-animation">
+    <img src="./images/wen-moon.jpg" alt="WEN MOON" class="w-100" />
+</section>
+<section class="cs-tokenomics">
+    <div class="cs-inner-container">
+        <h2 data-aos="zoom-in" data-aos-delay="100">TOKENOMICS</h2>
+        <p data-aos="zoom-in" data-aos-delay="100">
+            Zero resistance, all momentum. Join the $Moonshot journey and ride the $SOL rocket with us
+        </p>
+        <a class="cs-text-red" style="margin-top:20px" href="https://www.dextools.io/app/en/tron/pair-explorer/TCfcnrCxZapK8wZ4msbSZJ2UwAYvztQQSu?t=1724246614211">
+            https://www.dextools.io/app/en/tron/pair-explorer/TCfcnrCxZapK8wZ4msbSZJ2UwAYvztQQSu?t=1724246614211
+        </a>
+        <div
+                class="cs-tokenomics--image"
+                data-aos="zoom-in"
+                data-aos-delay="100"
+        >
+<!--            <video autoplay loop muted playsinline>-->
+<!--                <source src="./images/moonshot.mp4" type="video/mp4" />-->
+<!--            </video>-->
+
+            <!--                <img src="./images/trading.jpg" alt="" />-->
+        </div>
+        <div class="cs-tokenomics--content">
+            <div class="cs-tokenomics--cards">
+<!--                <div class="cs-arrow-image" data-aos="zoom-in" data-aos-delay="100">-->
+<!--                    <img src="./images/atwork-white.webp" alt="" />-->
+<!--                </div>-->
+                <div class="cs-card" data-aos="zoom-in" data-aos-delay="100">
+                    <h3>TOTAL SUPPLY</h3>
+                    <p>1,000,000,000 $MOONSHOT</p>
+                </div>
+                <div class="cs-card" data-aos="zoom-in" data-aos-delay="200">
+                    <h3>TOKEN ADDRESS</h3>
+                    <p>TLEoEmSpdHX2XQfJKqg1szY3YM7Adjhe19</p>
+                </div>
+            </div>
+            <div
+                    class="cs-tokenomics--image"
+                    data-aos="zoom-in"
+                    data-aos-delay="100"
+            >
+                <video autoplay loop muted playsinline>
+                    <source src="./images/moonshot.mp4" type="video/mp4" />
+                </video>
+
+                <!--                <img src="./images/trading.jpg" alt="" />-->
+            </div>
+        </div>
+
+    </div>
+</section>
+<section class="cs-network">
+    <div class="cs-inner-container">
+        <h2 data-aos="zoom-in">MOONSHOT only up</h2>
+        <p data-aos="zoom-in" data-aos-delay="200">
+
+            <br/>
+        </p>
+
+        <style>#dexscreener-embed{position:relative;width:100%;padding-bottom:125%;}@media(min-width:1400px){#dexscreener-embed{padding-bottom:65%;}}#dexscreener-embed iframe{position:absolute;width:100%;height:100%;top:0;left:0;border:0;}</style><div id="dexscreener-embed"><iframe src="https://dexscreener.com/tron/tcfcnrcxzapk8wz4msbszj2uwayvztqqsu?embed=1&theme=dark&trades=0&info=0"></iframe></div>
+
+        <a href="https://www.dextools.io/app/en/tron/pair-explorer/TCfcnrCxZapK8wZ4msbSZJ2UwAYvztQQSu?t=1724246614211" target="_blank" class="cs-main-button" data-aos="zoom-in" data-aos-delay="100">
+            BUY MOONSHOT HERE
+        </a>
+
+    </div>
+</section>
+<!--<section class="cs-gif-bg">
+  <img src="./images/pov.gif" alt="" />
+
+  <div class="cs-inner-container">
+    <div class="cs-badge">MOONSHOT TRADING POV</div>
+  </div>
+</section>-->
+<section class="cs-tokenomics cs-tokenomics-2" style="display: none;">
+    <div class="cs-inner-container">
+        <h2 data-aos="zoom-in" data-aos-delay="100">TOKENOMICS</h2>
+        <p data-aos="zoom-in" data-aos-delay="100">
+            No snipers, No taxes, No Mev. Woof on the $TRX train with us!
+        </p>
+<!--        <a style="margin-top:20px" href="https://www.dextools.io/app/en/tron/pair-explorer/TCfcnrCxZapK8wZ4msbSZJ2UwAYvztQQSu?t=1724246614211">-->
+<!--            https://www.dextools.io/app/en/tron/pair-explorer/TCfcnrCxZapK8wZ4msbSZJ2UwAYvztQQSu?t=1724246614211-->
+<!--        </a>-->
+        <div class="cs-tokenomics--content">
+            <div
+                    class="cs-tokenomics--image"
+                    data-aos="zoom-in"
+                    data-aos-delay="100"
+            >
+                <img src="./images/watching_the_launch.jpg" alt="" class="" />
+            </div>
+            <div class="cs-tokenomics--cards" data-aos="zoom-in">
+                <img src="./images/cartoon-xolo.gif" alt="" class="strobe-animation rocket-anim" />
+                <!--                <div class="cs-arrow-image" data-aos="zoom-in" data-aos-delay="100">
+                                    <img src="./images/rocket_launch.gif" alt="" />
+                                </div>
+                                <div class="cs-card" data-aos="zoom-in" data-aos-delay="100">
+                                    <h3>TOTAL SUPPLY</h3>
+                                    <p>1,000,000,000 $MOONSHOT</p>
+                                </div>
+                                <div class="cs-card" data-aos="zoom-in" data-aos-delay="200">
+                                    <h3>TOKEN ADDRESS</h3>
+                                    <p>TLEoEmSpdHX2XQfJKqg1szY3YM7Adjhe19</p>
+                                </div>
+                -->            </div>
+        </div>
+
+    </div>
+</section>
+<section class="cs-socials-section">
+    <div class="cs-socials--img">
+        <img src="./images/watching_the_launch.jpg" alt="Larry" />
+    </div>
+    <div                class="cs-socials--content"
+                        data-aos="zoom-up"
+                        data-aos-delay="100"
+    >
+        <div class="cs-inner-container">
+            <h2 data-aos="zoom-in" data-aos-delay="100">SOCIALS</h2>
+            <p data-aos="zoom-in" data-aos-delay="100">JOIN THE <span class="cs-text-red">$MOONSHOT</span> COMmUNITY</p>
+
+            <div class="cs-socials-icon-container">
+                <a class="cs-social-icon" href="https://x.com/moonshot"
+                ><img src="./images/x.png" alt=""
+                    /></a>
+                <a class="cs-social-icon" href="https://t.me/moonshotportal" target="_blank"
+                ><img src="./images/telegram.png" alt=""
+                    /></a>
+
+
+                <!--
+                 <div
+                   class="cs-socials--contact"
+                   data-aos="zoom-in"
+                   data-aos-delay="100"
+                 >
+                   <button class="cs-main-button">CLICK TO APE IN</button>
+                   <div class="cs-logo-container">
+                     <img src="./images/logo-text-black.webp" alt="" />
+                   </div>
+                 </div>-->
+            </div>
+        </div>
+    </div>
+</section>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init({
+        offset: 120, // offset (in px) from the original trigger point
+    });
+</script>
+</body>
+</html>
